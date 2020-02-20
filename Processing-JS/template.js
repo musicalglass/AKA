50357e3f0b402eb0ab9f9812898740c7fc38bef5
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2020-02-20 13:27:02 -0500

    New file made from template

--- Processing-JS/template.js
+++ Processing-JS/template.js
@@ -1,9 +1,17 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
 size(400, 400); 
 frameRate(30);
-
-
-//ProgramCodeGoesHere
-
-
-}};
+var angle = -11;
+var radius = 2;	
+var centerY = 200;
+var centerX =200;
+draw = function() {
+  frameRate(0);
+	var x = sin(angle) * radius;
+	var y = cos(angle) * radius;
+	fill(255,0, 0);
+	stroke(0,0, 0);
+	ellipse(x+200,y+200,40,40);
+	angle+=-16.5;
+	radius+=(0.05);
+};}};
