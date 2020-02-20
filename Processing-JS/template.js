54adb65dd73cd53d8c9b6a32f4f4fb3ce4f6974b
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2020-02-20 13:32:29 -0500

    Reverted template

--- Processing-JS/template.js
+++ Processing-JS/template.js
@@ -1,17 +1,9 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
+
+	
+
+
+//ProgramCodeGoesHere
 size(400, 400); 
 frameRate(30);
-var angle = -11;
-var radius = 2;	
-var centerY = 200;
-var centerX =200;
-draw = function() {
-  frameRate(0);
-	var x = sin(angle) * radius;
-	var y = cos(angle) * radius;
-	fill(255,0, 0);
-	stroke(0,0, 0);
-	ellipse(x+200,y+200,40,40);
-	angle+=-16.5;
-	radius+=(0.05);
-};}};
+}};
